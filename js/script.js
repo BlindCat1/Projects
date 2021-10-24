b0c19d1a98a7e0a67966321db0b05ff780ddcf5d
Max <blind1cat1@gmail.com>
2021-10-24 11:24:41 +0300

    add 2 answer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt("How many films you watch?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов', ''),
+      b = prompt('оценка', ''),
+      c = prompt('один из последних просмотренных фильмов', ''),
+      d = prompt('оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

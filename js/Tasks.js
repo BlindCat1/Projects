05d7ab3398d621d02e1f20e9abfbf097b3085b85
Max <blind1cat1@gmail.com>
2022-09-29 17:43:12 +0300

    new lab 29.09.2022

--- js/Tasks.js
+++ js/Tasks.js
@@ -64,3 +64,153 @@
 // }
 
 // console.log(result);
+
+//=============================================================================================
+
+
+// Место для первой задачи
+// function sayHello(name) {
+//     return ('Hello ' + name);
+
+// }
+
+// console.log(sayHello("Max"));
+
+// Место для второй задачи
+// function returnNeighboringNumbers(number) {
+//     return[number - 1 , number, number +1];
+
+// }
+
+// console.log(returnNeighboringNumbers(10));
+
+// Место для третьей задачи
+// function getMathResult(num, howMuch) {
+//     if (typeof(howMuch) !== "number" || howMuch <= 0){
+//         return(num);
+//     }
+
+//     let result = '';
+
+//     for(let i = 1; i <= howMuch ; i++){  
+//         if(i === howMuch){
+//             result += `${num * i}`;
+//          } else {
+//             result = result + num * i + "---";
+
+//         }
+//     }    
+//     console.log(result);
+//     return result;
+// }
+// getMathResult(3,3);
+
+//=============================================================================================
+
+// Место для первой задачи
+// function calculateVolumeAndArea(length) {
+//     if(typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)){
+//         console.log('При вычислении произошла ошибка');
+//         return'При вычислении произошла ошибка';
+//     }
+
+//     let volume = 0,
+//         area = 0;
+
+//     volume = length * length * length;
+//     area = (length * length) * 6;
+    
+//     console.log(`Объем куба: ${volume}, площадь всей поверхности: ${area}`);
+//     return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+// }
+
+// calculateVolumeAndArea(7);
+
+// Место для второй задачи
+// function getCoupeNumber(numberOfTicket) {
+//     if (typeof (numberOfTicket) !== 'number' || numberOfTicket < 0 || !Number.isInteger(numberOfTicket)) {
+//         return "Ошибка. Проверьте правильность введенного номера места";
+//     }
+
+//     if (numberOfTicket === 0 || numberOfTicket > 36) {
+//         return "Таких мест в вагоне не существует";
+//     }
+    
+//     console.log(Math.ceil(numberOfTicket / 4)); //Округление в большую сторону
+//     return Math.ceil(numberOfTicket / 4);
+    
+// }
+
+// getCoupeNumber(33);
+
+//=============================================================================================
+
+// Место для первой задачи
+// function getTimeFromMinutes(number) {
+//     if(typeof (number) !== 'number' || number < 0 || !Number.isInteger(number)){
+//         console.log("Ошибка, проверьте данные");
+//         return "Ошибка, проверьте данные";
+//     }
+
+//     let hour = Math.floor(number / 60); //Округление в меньшую сторону
+//     let minute = number % 60;
+//     let word = '';
+
+//     if(hour == 1){
+//         word = " час";
+//     } else if (hour == 2 || hour == 3 || hour == 4 ){
+//         word = ' часа';
+//     } else if (hour > 4 || hour ==0){
+//         word = ' часов';
+//     }
+
+//     console.log(`Это ${hour}${word} и ${minute} минут`);
+//     return `Это ${hour}${word} и ${minute} минут`;
+
+// }
+// getTimeFromMinutes(745);
+
+// Место для второй задачи
+// function findMaxNumber(first, second, third, forty) {
+//     if(typeof(first) !== 'number' ||
+//     typeof(second) !== 'number' ||
+//     typeof(third) !== 'number' ||
+//     typeof(forty) !== 'number') {
+//         console.log(0);
+//         return 0;
+//     } else{   
+//         console.log(Math.max(first,second,third,forty));
+//         return Math.max(first,second,third,forty);}
+// }
+
+// findMaxNumber(1, 5, 6.6, 10.5);
+// findMaxNumber(1, 5, '6', '10');
+
+//=============================================================================================
+
+// function fib(num) {
+//     if(typeof(num)!= 'number' || num <= 0 || !Number.isInteger(num)){
+//         console.log('');
+//         return '';
+//     }
+
+//     let result = '';
+//     let first = 0;
+//     let second = 1;
+
+//     for(let i = 0; i < num; i++){
+//         if (i + 1 === num) {
+//             result += `${first}`;
+//         } else {
+//             result += `${first} `;
+//         }
+
+//         let third = first + second;
+//         first = second;
+//         second = third;
+//     }
+//     console.log(result)
+//     return result;
+// }
+
+// fib(7);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many filmf you waching?", '');
+let numberOfFilms;
+
+function start (){
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,30 +18,54 @@ const personalMovieDB = {
     private: false
 };
 
-request: for (let i = 0; i < 2; i++){
-    const a = prompt("Last film what you wachin?", ''),
-          b = prompt("What you think about it?", '');
-    
-          if (a != null && b != null && a != '' && b != '' && a.length < 50 ){
-            personalMovieDB.movies [a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-            continue request;
+function remeberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt("Последний просмотренный фильм?", ''),
+              b = prompt("На сколько вы его оцените?", '');
+        
+              if (a != null && b != null && a != '' && b != '' && a.length < 50 ){
+                personalMovieDB.movies [a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+        }
     }
 }
-    
-if (personalMovieDB.count <10 && personalMovieDB.count !=0){
-     console.log('малоф ильмов');
-    }else if (personalMovieDB.count < 30 && personalMovieDB.count > 10){
-        console.log('Норм фильмов');
-    }else if (personalMovieDB>30){
-        console.log('Вы киноман');
-    }else {
-        console.log('Произошла ошибка');
-     }
-    
-console.log(personalMovieDB);
+
+remeberMyFilms();
+
+function detectPersonalLvl(){
+       
+    if (personalMovieDB.count <10 && personalMovieDB.count !=0){
+        console.log('Малоф ильмов');
+        }else if (personalMovieDB.count < 30 && personalMovieDB.count > 10){
+       console.log('Норм фильмов');
+        }else if (personalMovieDB>30){
+       console.log('Вы киноман');
+        }else {
+       console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLvl();
+
+function showMyDB(){
+    if(personalMovieDB.private == false){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGeners(){
+    for(let i = 1; i < 4; i++){
+        const a = prompt('Ваш любимый жанр под номером' + i + '?', '');
+        personalMovieDB.geners[i-1] = a;
+    }
+}
+
+writeYourGeners();
+
 
 
